8da8313ebd0468980b202815963cab5ebc55c6d1
Wojciech Kasperski <wojciech.kasperski97@gmail.com>
2019-09-08 18:40:35 +0200

    Update redirect page

--- src/config/auth.js
+++ src/config/auth.js
@@ -1,6 +1,6 @@
 const CLIENT_ID = "270fa00fa8144b2586c366cf69692f13";
 const SCOPES = encodeURIComponent("user-library-read");
-const REDIRECT_URI = encodeURIComponent("http://localhost:8080/");
+const REDIRECT_URI = process.env.NODE_ENV === "production" ? encodeURIComponent("https://suddenlypineapple.github.io/AlleSpot/") : encodeURIComponent("http://localhost:8080/");
 const URL = `https://accounts.spotify.com/authorize?client_id=${CLIENT_ID}&response_type=token&scope=${SCOPES}&redirect_uri=${REDIRECT_URI}`;
 
 export default URL;
